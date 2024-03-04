89999029c229106665e4d5ebea3cc51d5cd76523
Deepanbaalaji S <153833623+DeepanBaalaji25@users.noreply.github.com>
2024-03-04 20:06:48 +0530

    Create Solution.py

---
+++ Question 29/Solution.py
@@ -0,0 +1,7 @@
+import re
+
+for i in range(int(input())): 
+    match = re.findall(r"(\#[a-f0-9]{3,6})[\;\,\)]{1}", input(), re.I)
+    if match:
+        for j in list(match):
+            print(j)
